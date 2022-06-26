46b3c10b5798e8b0b66dcf6f5ae58b1f90d11b08
kiranraaj19 <kiranraajsekar@gmail.com>
2022-06-27 00:32:40 +0530

    added email part

--- src/Components/ContactPage.js
+++ src/Components/ContactPage.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Flex, Input, Textarea,Button } from '@chakra-ui/react'
+import { Flex, Input, Textarea, Button } from '@chakra-ui/react'
 import "./css/ContactPage.css"
 import { motion } from "framer-motion"
 
 function ContactPage() {
 
     return (
-        <motion.div className="container" initial = {{opacity:0}} animate={{opacity:1}} transition={{duration:1.5}}>
-            <Flex direction="column" gap="25px" w="60%" justify="center">
-                <Input placeholder="Name" focusBorderColor="green.200" />
-                <Input placeholder="Email" focusBorderColor="green.200" />
-                <Textarea placeholder="Message" focusBorderColor="green.200" />
-                <Button colorScheme='green'>Submit</Button>
-            </Flex>
+        <motion.div className="container" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5 }}>
+            <form action="https://formsubmit.co/kiranraajsekar@gmail.com" method="POST" class="form_container">
+                <Flex direction="column" gap="25px" w="60%" justify="center">
+                    <Input placeholder="Name" name="Name" focusBorderColor="green.200" />
+                    <Input placeholder="Email" name="Email" focusBorderColor="green.200" required/>
+                    <Textarea placeholder="Message" name="Textarea" focusBorderColor="green.200" />
+                    <Button type="submit" colorScheme='green'>Submit</Button>
+                </Flex>
+            </form>
         </motion.div>
     );
 }
--- src/Components/LinkedinIcon.js
+++ src/Components/LinkedinIcon.js
@@ -19,7 +19,7 @@ function LinkedinIcon() {
                     shadow='md'
                 >
                     <Flex justify="space-between">
-                        <p>Follow me on LinkedIn</p>
+                        <p>Follow me on <a onClick={()=>{window.location.href("www.linkedin.com/in/vaishnavi-padmanathan")}}>LinkedIn</a></p>
                         <GrClose color="white" onClick = {onToggle} size="2em"/>
                     </Flex>
                 </Box>
